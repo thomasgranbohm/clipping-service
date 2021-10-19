d74eaa62fd5c0805410933a673d75d2937099bcd
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-10-19 22:46:58 +0200

    Update Image sizes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	images: {
 		domains: urls,
 		minimumCacheTTL: 60,
-		deviceSizes: [450, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		deviceSizes: [450, 640, 750, 828, 1080, 1200],
 		imageSizes: [8, 16, 32, 48, 64, 96, 128, 256, 384],
 	},
 	publicRuntimeConfig: {
