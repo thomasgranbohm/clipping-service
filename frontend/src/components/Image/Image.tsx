5b55418f26d4b6db91b121bff75ee615ea37323d
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-11-14 17:24:25 +0100

    Updated Image component

--- frontend/src/components/Image/Image.tsx
+++ frontend/src/components/Image/Image.tsx
@@ -14,20 +14,15 @@ const Image = ({ className = '', src, alt, width, height }: ImageProps) => (
 	<div className={concat(classes['container'], className)}>
 		<NextImage
 			className={classes['image']}
-			src={src.replace(
-				process.env.NEXT_PUBLIC_BACKEND_URL,
-				'http://backend:1337'
-			)}
+			src={src.replace('localhost:1337', 'backend:1337')}
 			alt={alt}
 			width={width}
 			height={height}
 			layout="responsive"
 			placeholder="blur"
-			// blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0ntn5HwAEsgJWXUD+UAAAAABJRU5ErkJggg==`}
 			blurDataURL={`/_next/image?url=${encodeURIComponent(
-				src.replace(process.env.NEXT_PUBLIC_BACKEND_URL, 'http://backend:1337')
+				src.replace('localhost:1337', 'backend:1337')
 			)}&w=8&q=1`}
-			loading="lazy"
 		/>
 	</div>
 );
